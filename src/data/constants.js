2865c3bf8bee42d7496c9bc456e6fb5d48a92c75
mohdaameen <md.aameen2710@gmail.com>
2024-05-31 18:45:05 +0530

    Initial commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,25 +1,24 @@
 export const Bio = {
-  name: "Rishav Chanda",
+  name: "Mohd Aameen",
   roles: [
-    "Full Stack Developer",
-    "Android Developer",
-    "UI/UX Designer",
+    "AI/ML Engineer",
+    "FullStack Developer",
+    "Back-End Developer",
     "Programmer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/mohdaameen",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1UpkPnkHzStj_2RYLRY-6jZYfDmq_6JWr/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
   twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+  insta: "https://www.instagram.com/mohdaameeen227/"
 };
 
 export const skills = [
   {
-    title: "Frontend",
+    title: "AI/ML Engineer",
     skills: [
       {
         name: "React Js",
